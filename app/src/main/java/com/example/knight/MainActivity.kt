48cb72b4c5e579bb230c67695965f9c924c5cd53
pablopfx <a20pablofp1@iessanclemente.net>
2024-11-25 01:04:47 +0100

    basic homescreen

--- app/src/main/java/com/example/knight/MainActivity.kt
+++ app/src/main/java/com/example/knight/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.knight.ui.theme.KnightTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +43,63 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KnightTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppGame()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppGame() {
+    HomeScreen()
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    KnightTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Row (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp),
+            horizontalArrangement = Arrangement.End
+        ){
+            Button(
+                onClick = {},
+                modifier = Modifier
+            ) {
+                Icon(
+                    Icons.Filled.AddCircle,
+                    contentDescription = "Options",
+                    modifier = Modifier.size(35.dp)
+                )
+            }
+        }
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Knightly",
+                fontSize = 50.sp
+            )
+            Spacer(modifier = Modifier.height(100.dp) )
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Pulsa para comenzar", fontSize = 16.sp)
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun GameScreen() {
+    // la idea es hacer una barra bonita arriba con las monedas
+    // en el centro el monigote
+    // y abajo un botón para pulsar y las quests para ganar más monedas
 }
